f93f4fdced80639b0c6811d7cf9129460a587335
devivanova <119169640+devivanova@users.noreply.github.com>
2023-12-08 10:13:13 +0100

    added tasks

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,8 +3,8 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
-  return new Promise((resolve) => {
+function getPromise (): Promise<[string, number]> {
+  return new Promise((resolve): void => {
     resolve(['Text', 50]);
   });
 }
